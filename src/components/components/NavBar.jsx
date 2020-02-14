0f6fc6674c7a900d4eff860c12d80db621f29130
Yohannes Accrus <yohanesaccrus014@gmail.com>
2020-02-14 22:51:35 +0700

    Update NavBar.jsx

--- src/components/components/NavBar.jsx
+++ src/components/components/NavBar.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from 'react'
 
 const NavBar = ({ sortList }) => {
     return (
-        <nav className="navbar container">
-            <div className="navbar__logo"><span>qoala</span> test</div>
-            <div className="navbar__buttons">
-                <button className="btn btn--orange"
-                    onClick={() => sortList("age")}
-                ><i className="fas fa-tint"></i> color</button>
-                <button className="btn btn--blue"
-                    onClick={() => sortList("city")}
-                ><i className="fas fa-city"></i> city</button>
+        <nav className={'navbar container'}>
+            <div className={'navbar__logo'}><span>qoala</span> test</div>
+            <div className={'navbar__buttons'}>
+                <button className={'btn btn--orange'}
+                    onClick={() => sortList('age')}
+                ><i className={'fas fa-tint'}></i> color</button>
+                <button className={'btn btn--blue'}
+                    onClick={() => sortList('city')}
+                ><i className={'fas fa-city'}></i> city</button>
             </div>
         </nav>
     )
