d593af09ace683a776646182f9618104d1804053
Yohannes Accrus <yohanesaccrus014@gmail.com>
2020-02-14 22:55:29 +0700

    Update App.jsx

--- src/components/container/App.jsx
+++ src/components/container/App.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import axios from "axios";
-import _ from 'lodash';
-import { Card, NavBar } from "../components/Components";
+import React, { Component } from 'react'
+import axios                from 'axios'
+import _                    from 'lodash'
+import { Card, NavBar }     from '../components/Components'
 
 class App extends Component {
     state = {
@@ -10,7 +10,7 @@ class App extends Component {
 
     loadUser = () => {
         const { users } = this.state
-        const url = "https://randomuser.me/api/?results=10"
+        const url = 'https://randomuser.me/api/?results=10'
         axios.get(url)
             .then(response => {
                 this.setState({
@@ -31,9 +31,9 @@ class App extends Component {
     }
 
     handleScrollMobile = () => {
-        let lastData = document.querySelector("div.cards > div.card:last-child");
-        let lastDataOffset = lastData.offsetTop + lastData.clientHeight;
-        let pageOffset = window.pageYOffset + window.innerHeight;
+        let lastData        = document.querySelector('div.cards > div.card:last-child')
+        let lastDataOffset  = lastData.offsetTop + lastData.clientHeight
+        let pageOffset      = window.pageYOffset + window.innerHeight
 
         if (pageOffset > lastDataOffset) {
             this.loadMore()
@@ -41,9 +41,9 @@ class App extends Component {
     }
 
     handleScrollWeb = () => {
-        let lastData = document.querySelector("div.cards > div.card:last-child");
-        let lastDataOffset = lastData.offsetLeft + lastData.clientWidth;
-        let pageOffset = window.pageXOffset + window.innerWidth;
+        let lastData        = document.querySelector('div.cards > div.card:last-child')
+        let lastDataOffset  = lastData.offsetLeft + lastData.clientWidth
+        let pageOffset      = window.pageXOffset + window.innerWidth
 
         if (pageOffset > lastDataOffset) {
             this.loadMore()
@@ -53,7 +53,7 @@ class App extends Component {
     componentDidMount() {
         let initialUsers = JSON.parse(localStorage.getItem('initialUsers'));
         if (!initialUsers || initialUsers.length < 1) {
-            this.loadUser();
+            this.loadUser()
         } else {
             this.setState({
                 users: initialUsers
@@ -61,11 +61,11 @@ class App extends Component {
         }
 
         if (window.innerWidth >= 768) {
-            this.scrollListener = window.addEventListener("scroll", e => {
+            this.scrollListener = window.addEventListener('scroll', e => {
                 this.handleScrollWeb(e)
             })
         } else {
-            this.scrollListener = window.addEventListener("scroll", e => {
+            this.scrollListener = window.addEventListener('scroll', e => {
                 this.handleScrollMobile(e)
             })
         }
@@ -73,36 +73,36 @@ class App extends Component {
 
     defineCardColor = (age) => {
         if (age < 21) {
-            return "red";
+            return 'red'
         } else if (age > 56) {
-            return "blue";
+            return 'blue'
         } else {
-            return "green";
+            return 'green'
         }
     }
 
     sortList = (key) => {
-        let arrayCopy = [...this.state.users];
-        if (key === "city") {
-            const myOrderedArray = _.sortBy(arrayCopy, `location.${key}`);
+        let arrayCopy = [...this.state.users]
+        if (key === 'city') {
+            const myOrderedArray = _.sortBy(arrayCopy, `location.${key}`)
             this.setState({ users: myOrderedArray }, () => {
                 localStorage.setItem('initialUsers', JSON.stringify(this.state.users))
-            });
+            })
         } else {
-            const myOrderedArray = _.sortBy(arrayCopy, `dob.${(key)}`);
+            const myOrderedArray = _.sortBy(arrayCopy, `dob.${(key)}`)
             this.setState({ users: myOrderedArray }, () => {
                 localStorage.setItem('initialUsers', JSON.stringify(this.state.users))
-            });
+            })
         }
-    };
+    }
 
     render() {
-        const { users } = this.state;
+        const { users } = this.state
         return (
-            <div className="app">
+            <div className={'app'}>
                 <NavBar sortList={this.sortList} />
-                <div className="home container">
-                    <div className="cards">
+                <div className={'home container'}>
+                    <div className={'cards'}>
                         {
                             users.map(user => (
                                <Card user={user} defineColor={this.defineCardColor} key={user.login.username}/>
