84cd2d600b3ae11518468512d1bbfd1038b63e6f
Yohannes Accrus <yohanesaccrus014@gmail.com>
2020-02-14 22:56:19 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React              from 'react'
+import ReactDOM           from 'react-dom'
+import App                from './components/container/App'
+import * as serviceWorker from './serviceWorker'
 import "./assets/sass/main.scss";
-import App from './components/container/App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render( < App / > , document.getElementById('root'));
 
